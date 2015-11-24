07d301acb266cd9a515b6d8310ee2b50ae88196e
Ikeaner | Tom Quinders <tomquinders@gmail.com>
2015-11-24 19:06:37 +0100

    Grundstruktur mit wenigen Basisbeziehungen aufgebaut

--- Labyrinth_Projekt/src/TEST.java
+++
@@ -1,4 +0,0 @@
-
-public class TEST {
- //Ne Testklasse! Da werde ich ja gleich ganz rattig.
-}
---
+++ Labyrinth_Projekt/src/data/Spieler.java
@@ -0,0 +1,20 @@
+/*
+ * Die Spieler Klasse beinhält Informationen über den Spieler.
+ * @author
+ * @version 0.1
+ */
+package data;
+
+public class Spieler
+{
+	private Position currentPosition;
+	private int currentScore;
+	private Inventar inv;
+	
+	public Spieler(Position p)
+	{
+		currentPosition = p;
+		currentScore = 0;
+		inv = new Inventar();
+	}
+}
