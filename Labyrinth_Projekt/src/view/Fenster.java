847d4d770a29c44804ebb0143753bf1fc827a937
Jaqueline Timmermann <jace2807@gmail.com>
2015-12-10 17:50:53 +0100

    das sind eindeutig zu viele für zu wenig code... ich geh essen :D

--- Labyrinth_Projekt/src/view/Fenster.java
+++ Labyrinth_Projekt/src/view/Fenster.java
@@ -15,12 +15,13 @@ public class Fenster
 	public Fenster()
 	{
 		//TODO: Konstruktor 
+		//öffnet ein Fenster mit dem Titel Labyrinth, Fenster schließt sich wenn man es wegklickt
 		JFrame frm = new JFrame();
 		frm.setTitle("Labyrinth");
 
 		frm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frm.setSize(300,200);
+		frm.setSize(1200,800);
 		frm.setLocation(50,50);
 		frm.setVisible(true);
 	}		
